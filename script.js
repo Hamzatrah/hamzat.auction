65449b9472309d6651779d7474fbe30f153d7486
Hamzatrah <41709149+Hamzatrah@users.noreply.github.com>
2023-08-22 00:20:28 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 var random = new Array();
 
 random[0] = "mailto:contact@hamzat.xyz";
-random[1] = "https://www.instagram.com/hamzat.auction/";
+random[1] = "mailto:chief@hamzat.xyz";
+random[2] = "mailto:him@hamzat.xyz";
+random[3] = "mailto:him@hamzat.xyz";
 
 function randomlink() {
   window.location = random[Math.floor(Math.random()*random.length)];
